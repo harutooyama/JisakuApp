bc13c096f32d812eea293d89fed66d879f0aa744
Owner <owner@MA3001.local>
2020-06-26 12:20:57 +0900

    added JisauApp

--- Jisaku/Jisaku/ViewController.swift
+++ Jisaku/Jisaku/ViewController.swift
@@ -17,6 +17,7 @@ class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate
     var cellObjectId:String = ""
     let uuid = UIDevice.current.identifierForVendor?.uuidString
     var userId:String = ""
+    var re2:Bool! = nil
     
     
     @IBOutlet weak var tableView: UITableView!
@@ -26,7 +27,13 @@ class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate
         tableView.delegate = self
         tableView.dataSource = self
         userCheck()
-        fetchThread()
+        DispatchQueue.global().async{
+            self.deleteThreadAndResponse()
+            
+        }
+        wait({return self.re2 == nil}){
+            self.fetchThread()
+        }
     }
 
     func fetchThread(){
@@ -180,7 +187,6 @@ class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate
 
         // objectIdプロパティを設定
         object.objectId = objectId
-        print(object.objectId)
         
         // データストアから削除
         object.deleteInBackground({(result) in
@@ -212,39 +218,119 @@ class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate
             }
         }
     }
-    /*
+    
     func deleteThreadAndResponse(){
-        let query = NCMBQuery(className: "users")
+        print("デリート")
+        let query = NCMBQuery(className: "thread")
+        let query1 = NCMBQuery(className: "response")
         let now = NSDate()
         let formatter = DateFormatter()
-        var objectIdList:[String]
+        var deleteThreads = NSArray()
+        var deleteResponse = NSArray()
+        var createDate:String = ""
+        var deleteListPointer:[Int] = []
+        var deleteThreadsObjectIdList:[String] = []
+        var deleteResponseObjectIdList:[String] = []
+        var re:Bool! = nil
+        var re1:Bool! = nil
         formatter.dateFormat = "yyyy/MM/dd"
         let nowDate = formatter.string(from: now as Date)
-        createDate = (objects![0] as AnyObject).object(forKey:"createDate") as! String
-        createDate = createDate.prefix(10).replacingOccurrences(of: "-", with: "/")
+        
         DispatchQueue.global().async {
+            
             query!.findObjectsInBackground({(objects, error) in
                 if (error == nil) {
+                   
                     if(objects!.count > 0) {
-                        self.userId = (objects![0] as AnyObject).object(forKey:"") as! String
-                        objectId = (objects![0] as AnyObject).objectId
-                        createDate = (objects![0] as AnyObject).object(forKey:"createDate") as! String
-                        createDate = createDate.prefix(10).replacingOccurrences(of: "-", with: "/")
-                        ret = false
+                        deleteThreads = objects! as NSArray
+                        
+                        for i in 0..<deleteThreads.count{
+                            let thr = deleteThreads.object(at: i) as! NCMBObject
+                            createDate = (thr.object(forKey: "createDate") as? String)!
+                            createDate = createDate.prefix(10).replacingOccurrences(of: "-", with: "/")
+                                
+                            if(createDate != nowDate){
+                                deleteListPointer.append(i)
+                                deleteThreadsObjectIdList.append(thr.objectId)
+                                
+                            }
+                        }
+                    }
+                } else {
+                    print(error!.localizedDescription)
+                }
+            })
+            query1!.findObjectsInBackground({(objects, error) in
+                print("asdfafa")
+                if (error == nil) {
+                    if(deleteThreads.count > 0) {
+                        print("レス削除始まり")
+                        deleteResponse = objects! as NSArray
+                        if(deleteResponseObjectIdList.count > 0){
+                            for p in deleteListPointer {
+                                let res = deleteResponse.object(at: p) as! NCMBObject
+                                deleteResponseObjectIdList.append(res.objectId)
+                            }
+                        }else{
+                            self.re2 = true
+                        }
+                        re = true
                     }else{
-                        ret = true
+                        re = false
+                        self.re2 = true
                     }
                 } else {
                     print(error!.localizedDescription)
                 }
             })
         }
-        if createDate
-            var response = NSArray()
-        
+        wait( { return re == nil } ) {
+            if(re == true){
+                DispatchQueue.global().async{
+                    let thrObject : NCMBObject = NCMBObject(className: "thread")
+                    for thrid in deleteThreadsObjectIdList{
+                        thrObject.objectId = thrid
+                        print(thrObject.objectId)
+                        thrObject.deleteInBackground({(result) in
+                            if( result == nil){
+                                print("Thread削除に成功しました")
+                                
+                            }else{
+                                print(thrObject.objectId)
+                                print("削除に失敗しました: \(result)")
+                               
+                            }
+                            if(thrid == deleteThreadsObjectIdList[deleteThreadsObjectIdList.count - 1]){
+                                re1 = true
+                            }
+                        })
+                    }
+                }
+                self.wait({return re1 == nil}){
+                    DispatchQueue.global().async{
+                        let resObject : NCMBObject = NCMBObject(className: "response")
+                        for resid in deleteResponseObjectIdList{
+                            resObject.objectId = resid
+                            print(resObject.objectId)
+                            resObject.deleteInBackground({(result) in
+                                if( result == nil){
+                                    print("Response削除に成功しました")
+                                    
+                                }else{
+                                    print("削除に失敗しました: \(result)")
+                                    
+                                }
+                                if(deleteResponseObjectIdList.count == 0 || resid == deleteResponseObjectIdList[deleteResponseObjectIdList.count-1]){
+                                    self.re2 = true
+                                }
+                            })
+                            
+                        }
+                    }
+                }
+            }
+        }
     }
- */
-    
 }
     
 
