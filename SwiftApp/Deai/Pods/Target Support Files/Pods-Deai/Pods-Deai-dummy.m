55f6dc79f1c1c0a84beae00ed10d0e962344664b
Owner <owner@MA3001.local>
2020-06-26 12:30:13 +0900

    remove SwiftApp

--- SwiftApp/Deai/Pods/Target Support Files/Pods-Deai/Pods-Deai-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Deai : NSObject
-@end
-@implementation PodsDummy_Pods_Deai
-@end
--- SwiftApp/Pedometer/Pods/Target Support Files/Pods-Pedometer/Pods-Pedometer-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Pedometer : NSObject
-@end
-@implementation PodsDummy_Pods_Pedometer
-@end
